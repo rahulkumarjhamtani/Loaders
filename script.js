9175e3040c0b174a137f25525df1bd5157a859bd
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-09-06 09:47:03 +0530

    loading

---
+++ script.js
@@ -0,0 +1,9 @@
+var preloader1 = document.getElementById("stopspin");
+var preloader2 = document.getElementById("stopspin");
+var preloader3 = document.getElementById("stopspin");
+    function loadpage() {
+        preloader.style.display = 'none';
+        preloader.style.display = 'none';
+        preloader.style.display = 'none';
+        location. href = "https://rahulkumarjhamtani.github.io/Notes/";
+    }
